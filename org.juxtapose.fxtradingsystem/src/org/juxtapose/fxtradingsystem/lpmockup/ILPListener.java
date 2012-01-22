1a47a13e28ef9890845b18f064f7eed4df0e9448
Pontus Jorgne <p_jorgne@hotmail.com>
2012-01-22 19:12:32 +0100

    remove LP mockup

--- org.juxtapose.fxtradingsystem/src/org/juxtapose/fxtradingsystem/lpmockup/ILPListener.java
+++
@@ -1,6 +0,0 @@
-package org.juxtapose.fxtradingsystem.lpmockup;
-
-public interface ILPListener
-{
-	public void priceUpdate( String inCcy1, String inCcy2, String inPeriod, Double inBid, Double inAsk );
-}
